1e4637876eda008111fdaf44663ec14dfc20b7d6
Sophia Capello <scapello@mymail.mines.edu>
2020-02-13 13:03:45 -0700

    recentered mouth

--- src/Cat.java
+++ src/Cat.java
@@ -17,7 +17,7 @@ public class Cat {
 	private static final int MOUTH_HEIGHT = 20;
 	private static final int MOUTH_WIDTH = HEAD_DIMENSION/2;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
+	private static final int MOUTH_X = HEAD_DIMENSION/7 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
 	
 	// draw will render the Cat on the Graphics object
